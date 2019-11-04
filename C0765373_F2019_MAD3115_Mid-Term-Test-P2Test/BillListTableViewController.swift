48160cf28b8be354fc35c59dda740e5bd22696b4
sachinmahi <sachinmahi>
2019-11-04 01:28:37 -0500

    creating files

---
+++ C0765373_F2019_MAD3115_Mid-Term-Test-P2Test/BillListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillListTableViewController.swift
+//  C0765373_F2019_MAD3115_Mid-Term-Test-P2Test
+//
+//  Created by simar mahi on 04/11/19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class BillListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
