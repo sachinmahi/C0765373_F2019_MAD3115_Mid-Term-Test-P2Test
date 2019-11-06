ff372eb63b791643441131c092e3b42142037bb0
sachinmahi <sachinmahi>
2019-11-06 01:37:06 -0500

    ShowBillDetailsViewController

---
+++ C0765373_F2019_MAD3115_Mid-Term-Test-P2Test/ShowBillDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowBillDetailsViewController.swift
+//  C0765373_F2019_MAD3115_Mid-Term-Test-P2Test
+//
+//  Created by simar mahi on 06/11/19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ShowBillDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
