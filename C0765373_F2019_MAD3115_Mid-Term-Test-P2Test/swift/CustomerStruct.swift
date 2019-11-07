28b0dc4abd776b36158dfac14ad166e7ff48edba
sachinmahi <sachinmahi>
2019-11-07 18:33:31 -0500

    customerstruct

--- C0765373_F2019_MAD3115_Mid-Term-Test-P2Test/swift/CustomerStruct.swift
+++ C0765373_F2019_MAD3115_Mid-Term-Test-P2Test/swift/CustomerStruct.swift
@@ -1,4 +1,10 @@
-
+//
+//  CustomerStruct.swift
+//  C0765373_F2019_MAD3115_Mid-Term-Test-P2Test
+//
+//  Created by simar mahi on 04/11/19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
 import Foundation
 
 struct CustomersStruct {
